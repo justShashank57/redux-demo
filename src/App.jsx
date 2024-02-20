d592d5d4cc944349d3407ef24b2130bca6aab44a
justShashank57 <justShashank57@gmail.com>
2024-02-20 17:27:00 +0530

    Reduc demo completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlice";
+export default function App() {
+  const count = useSelector((state)=>state.counter.value)
+  const dispatch = useDispatch();
+  return (
+    <div className="App" style={{display:"flex",flexDirection:"column",width:"50%"}}>
+          Hi this is a counter.
+          <button onClick={()=>dispatch(decrement())}>
+            Decrement
+          </button>
+          <span style={{width:"100%",textAlign:"center"}}>{count}</span>
+          <button onClick={()=>dispatch(increment())}>
+            Increment
+          </button>
+    </div>
+  );
+}
